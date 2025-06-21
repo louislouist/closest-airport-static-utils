4edbf84014a5832e50e31518db3bc0f67347b143
Louis Louis <louis.gh@nuxsys.com>
2025-06-21 15:19:30 -0400

    Update findClosesAirpots to take [] for all airports type search. Update package.json to 1.0.1

--- src/findClosestAirports.ts
+++ src/findClosestAirports.ts
@@ -8,7 +8,7 @@ export function findClosestAirports(
 	allowedTypes: string[] = ['large_airport', 'medium_airport']
 ): Airport[] {
 	return airports
-		.filter(airport => allowedTypes.includes(airport.type))
+		.filter(airport => allowedTypes.length === 0 || allowedTypes.includes(airport.type))
 		.map(airport => ({
 			...airport,
 			distance: haversineDistance(lat, lon, airport.lat, airport.lon)
@@ -16,3 +16,6 @@ export function findClosestAirports(
 		.sort((a, b) => a.distance - b.distance)
 		.slice(0, count);
 }
+
+// NOTE: Current Airport Types
+// ['balloonport', 'closed', 'heliport', 'large_airport', 'medium_airport', 'seaplane_base', 'small_airport']
